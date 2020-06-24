2a12c2dbf4ec4abcd289848e784d9214de36d278
Rodrigo T. Guerra <rodrigotguerra2@gmail.com>
2020-06-24 10:24:04 -0300

    Implementação inicial do método que imprime o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using xadrez_console.tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,10 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            
+            Tabuleiro tab = new Tabuleiro(8, 8);
+
+            Tela.imprimirTabuleiro(tab);
+
         }
     }
 }
